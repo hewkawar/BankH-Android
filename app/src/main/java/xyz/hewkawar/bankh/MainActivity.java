f2c915a601607e6d236c23682e52fde780fdc8af
HewkawAr <earthwangsaeng2@gmail.com>
2023-11-27 22:34:48 +0700

    v.1.0.0

---
+++ app/src/main/java/xyz/hewkawar/bankh/MainActivity.java
@@ -0,0 +1,70 @@
+package xyz.hewkawar.bankh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        webView = findViewById(R.id.webview);
+
+        // Enable JavaScript (optional)
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        // Set a custom user agent
+        String userAgent = "HewkawArApplicationService (Android/1.0)";
+        webSettings.setUserAgentString(userAgent);
+
+        // Set a WebViewClient to handle clicks within the WebView
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                // Handle the redirect here if needed
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+
+        // Load a URL (e.g., Google)
+        webView.loadUrl("https://bank.hewkawar.xyz/login");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
